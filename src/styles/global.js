96230ca652e6f3a8821573a18fc0403fa8cdd0d7
bhyago <bhyago@gmail.com>
2020-02-24 16:19:04 -0300

    estilos globais

---
+++ src/styles/global.js
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+ @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
+
+ * {
+  margin: 0;
+  padding: 0;
+  outline: 0;
+  box-sizing: border-box;
+}
+
+*:focus {
+  outline: 0;
+}
+
+html, body, #root {
+  height: 100%;
+}
+
+body {
+  -webkit-font-smootthing: antialiased !important
+}
+
+body, input, button {
+  font: 14px Roboto, sans-serif;
+}
+
+a {
+  text-decoration: none;
+}
+
+ul {
+  list-style: none;
+}
+
+/* #root {
+  max-width: 1020px;
+  margin: 0 auto;
+  padding: 0 20px 50px;
+} */
+
+button {
+  cursor: pointer;
+}
+
+`;
